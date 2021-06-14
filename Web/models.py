09cefe75ba74b6f4575f4af6d272c86ca7fd682d
Tyrion1018 <joega068472@gmail.com>
2021-06-14 23:51:42 +0800

    init commit

---
+++ Web/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+
+
+# Create your models here.
+class Customer(models.Model):
+
+    name = models.CharField(max_length=50)
+    GUID = uuid.uuid4()
+
+    def __str__(self):
+        return self.name
+
+    class Meta(object):
+        db_table = "Customer"
--- Web/settings.py
+++ Web/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-q)(p1+k&^jvs^*$cu1zp#3*k1u(!k%h@wzh124-_8e!e_af9+5
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -54,7 +52,7 @@ ROOT_URLCONF = 'Web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,17 +67,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Web.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'WebRepository',
+        'USER': 'sa',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'OPTIONS': {'driver': 'ODBC Driver 17 for SQL Server', },
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -112,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
