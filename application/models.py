23a21b619f6d63342b07fd6ca3c51efd393a58ce
Tyrion1018 <joega068472@gmail.com>
2021-06-19 23:06:16 +0800

    Add category and item database

--- application/models.py
+++ application/models.py
@@ -1,8 +1,36 @@
 from django.db import models
 
+content = "{promissoryAmount:, promissoryNote:, nationalID:, Address:, " \
+          "promissoryAddress:,promissoryDate:,promissoryLastDate:, promissoryNoticeDate:, promissoryCourt:," \
+          "Date:}"
+
 
 class Consumer(models.Model):
-    LoginID = models.CharField(u'LineID', max_length=20)
-    Name = models.CharField(u'姓名', max_length=10)
-    Sex = models.BooleanField(default=True)
-    email = models.CharField(max_length=50, null=True)
+    OrderID = models.TextField()
+    LoginID = models.TextField(primary_key=True, verbose_name="LineID")
+    Name = models.TextField(default="", verbose_name="請輸入全名")
+    email = models.TextField(max_length=50, default="", verbose_name="ex:xxxx@gmail.com", blank=True)
+    phone = models.TextField(max_length=10, default="", verbose_name="ex:0912345678")
+    timeStamp = models.DateTimeField()
+
+    class Meta(object):
+        db_table = "tblCustomer"
+
+
+class ApplicationItem(models.Model):
+    itemID = models.TextField(primary_key=True)
+    name = models.TextField()
+    content = models.TextField()
+
+    class Meta(object):
+        db_table = "tblApplicationItem"
+
+
+class ApplicationCategory(models.Model):
+    categoryID = models.TextField(primary_key=True)
+    category = models.TextField()
+    item = models.ForeignKey(ApplicationItem, on_delete=models.CASCADE)
+
+    class Meta(object):
+        db_table = "tblApplicationCategory"
+
---
+++ infrastructure/components/methods.py
@@ -0,0 +1,9 @@
+import json
+
+
+def convert_to_json(data):
+    return json.dumps(data.__dict__)
+
+
+def convert_from_json(data):
+    return json.loads(data)
